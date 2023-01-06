a01f658b89c2e6fdc029960eaef4e97d18283211
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2023-01-06 08:21:14 +0700

    code splitting

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,3 +29,99 @@ exports.protect = async (req, res, next) => {
     return res.status(501).json({ success: false, message: error });
   }
 };
+
+exports.login = async (req, res) => {
+  if (!req.headers.authorization) {
+    return res
+      .status(401)
+      .send({ message: "You are not logged in! Please login to get access" });
+  }
+
+  const token = req.headers.authorization.split(" ")[1];
+  // decode token
+  try {
+    const decodeValue = await admin.auth().verifyIdToken(token);
+    if (!decodeValue) {
+      return res
+        .status(401)
+        .json({ message: "You are not logged in! Please login to get access" });
+    } else {
+      // Checking user exists or not
+      const userExists = await User.findOne({ userId: decodeValue.user_id });
+      if (!userExists) {
+        // await User.create()
+        createNewUser(decodeValue, req, res);
+      } else {
+        updateUser(decodeValue, req, res);
+      }
+    }
+  } catch (error) {
+    return res.status(501).json({ message: error });
+  }
+};
+
+const createNewUser = async (decodeValue, req, res) => {
+  const newUser = {
+    name: decodeValue.name,
+    email: decodeValue.email,
+    imageURL: decodeValue.picture,
+    userId: decodeValue.user_id,
+    emailVerified: decodeValue.email_verified,
+    role: "member",
+    authTime: decodeValue.auth_time,
+    likedSongs: [],
+  };
+
+  try {
+    const savedUser = await User.create(newUser);
+    res.status(200).json({ success: true, data: savedUser });
+  } catch (error) {
+    res.status(501).send({ success: false, message: error });
+  }
+};
+
+const updateUser = async (decodeValue, req, res) => {
+  const filter = { userId: decodeValue.user_id };
+
+  const option = {
+    new: true,
+    runValidators: true,
+  };
+  try {
+    const result = await User.findOneAndUpdate(
+      filter,
+      {
+        authTime: decodeValue.auth_time,
+        ...req.body,
+      },
+      option
+    );
+    result
+      ? res.status(200).json({ data: result })
+      : res
+          .status(400)
+          .json({ success: false, message: "Data not found with that ID" });
+  } catch (error) {
+    return res.status(501).json({ success: false, message: error });
+  }
+};
+
+exports.updateMe = async function updateMe(req, res, next) {
+  const user = await User.findOneAndUpdate(
+    { userId: req.user.userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  user
+    ? res.status(200).json({ data: user })
+    : res
+        .status(400)
+        .json({ success: false, message: "Data not found with that ID" });
+};
+
+exports.getLikedSongs = async function (req, res, next) {
+  const likedSongs = await User.findOne({ userId: req.user.userId })
+    .select("likedSongs -_id")
+    .populate("likedSongs");
+  res.status(200).json({ success: true, data: likedSongs });
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,106 +1,14 @@
 const express = require("express");
-const admin = require("../config/firebase.config");
-const auth = require("../controllers/authController");
-const User = require("../models/userModel");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.get("/login", async (req, res) => {
-  if (!req.headers.authorization) {
-    return res
-      .status(401)
-      .send({ message: "You are not logged in! Please login to get access" });
-  }
+router.get("/login", authController.login);
 
-  const token = req.headers.authorization.split(" ")[1];
-  // decode token
-  try {
-    const decodeValue = await admin.auth().verifyIdToken(token);
-    if (!decodeValue) {
-      return res
-        .status(401)
-        .json({ message: "You are not logged in! Please login to get access" });
-    } else {
-      // Checking user exists or not
-      const userExists = await User.findOne({ userId: decodeValue.user_id });
-      if (!userExists) {
-        // await User.create()
-        createNewUser(decodeValue, req, res);
-      } else {
-        updateUser(decodeValue, req, res);
-      }
-    }
-  } catch (error) {
-    return res.status(501).json({ message: error });
-  }
-});
+router
+  .route("/updateMe")
+  .patch(authController.protect, authController.updateMe);
 
-const createNewUser = async (decodeValue, req, res) => {
-  const newUser = {
-    name: decodeValue.name,
-    email: decodeValue.email,
-    imageURL: decodeValue.picture,
-    userId: decodeValue.user_id,
-    emailVerified: decodeValue.email_verified,
-    role: "member",
-    authTime: decodeValue.auth_time,
-    likedSongs: [],
-  };
-
-  try {
-    const savedUser = await User.create(newUser);
-    res.status(200).json({ success: true, data: savedUser });
-  } catch (error) {
-    res.status(501).send({ success: false, message: error });
-  }
-};
-
-const updateUser = async (decodeValue, req, res) => {
-  const filter = { userId: decodeValue.user_id };
-
-  const option = {
-    new: true,
-    runValidators: true,
-  };
-  try {
-    const result = await User.findOneAndUpdate(
-      filter,
-      {
-        authTime: decodeValue.auth_time,
-        ...req.body,
-      },
-      option
-    );
-    result
-      ? res.status(200).json({ data: result })
-      : res
-          .status(400)
-          .json({ success: false, message: "Data not found with that ID" });
-  } catch (error) {
-    return res.status(501).json({ success: false, message: error });
-  }
-};
-
-async function updateMe(req, res, next) {
-  const user = await User.findOneAndUpdate(
-    { userId: req.user.userId },
-    req.body,
-    { new: true, runValidators: true }
-  );
-  user
-    ? res.status(200).json({ data: user })
-    : res
-        .status(400)
-        .json({ success: false, message: "Data not found with that ID" });
-}
-
-router.route("/updateMe").patch(auth.protect, updateMe);
-
-router.get("/likedSongs", auth.protect, async function (req, res, next) {
-  const likedSongs = await User.findOne({ userId: req.user.userId })
-    .select("likedSongs -_id")
-    .populate("likedSongs");
-  res.status(200).json({ success: true, data: likedSongs });
-});
+router.get("/likedSongs", authController.protect, authController.getLikedSongs);
 
 module.exports = router;
